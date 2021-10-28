647ccc32d48238609f2ad48eaaa4416b97ba96e2
Matt Tedder <mjtedder@icloud.com>
2021-10-28 14:36:03 -0600

    Changes server port, adds cors middleware.

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
+const allowedOrigins = ['http://localhost:8080'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+
+app.use(cors(corsOptions));
 app.use(express.static('src'));
 app.use(express.json({ limit: '1mb' }));
 
